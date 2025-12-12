776376757432e368837cb027b0da5694bbf8ef31
Ebenz Augustave <143364444+augustave@users.noreply.github.com>
2025-12-12 16:42:37 -0500

    Add site build config for Vercel

---
+++ vite.site.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: false,
+  },
+});
